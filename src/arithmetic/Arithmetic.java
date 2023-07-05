7f78b2bfe0caae340bb16f938ae4376579a8aa20
jainisheridan <topiwalj@sheridancollege.ca>
2023-07-05 17:06:37 -0400

    Updated code

--- src/arithmetic/Arithmetic.java
+++ src/arithmetic/Arithmetic.java
@@ -15,22 +15,64 @@ import static java.time.Clock.system;
  * @author sivagamasrinivasan
  * 
  */
-public class Arithmetic 
-{
+enum Operation {
+    PLUS,
+    MINUS,
+    TIMES,
+    DIVIDE
+}
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) 
-    {
-       
-        ArithmeticBase r= new ArithmeticBase();
-        Scanner in= new Scanner(System.in);
-        int n= in.nextInt();
-        int m= in.nextInt();
-        double result = r.calculate(m,n);
-        System.out.println("result :" +result); 
+class ArithmeticInputOutput {
+    public Operation getOperationFromUser() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter arithmetic operation to perform: ");
+        String operationString = scanner.next().toUpperCase();
+        
+        try {
+            return Operation.valueOf(operationString);
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException("Invalid operation: " + operationString);
+        }
+    }
     
+    public int getNumberFromUser() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        return scanner.nextInt();
+    }
+    
+    public void displayResult(double result) {
+        System.out.println("Result: " + result);
     }
 }
 
+class ArithmeticCalculator {
+    public double calculate(double x, double y, Operation operation) {
+        switch (operation) {
+            case PLUS:
+                return x + y;
+            case MINUS:
+                return x - y;
+            case TIMES:
+                return x * y;
+            case DIVIDE:
+                return x / y;
+            default:
+                throw new AssertionError("Unknown operation: " + operation);
+        }
+    }
+}
+
+public class Arithmetic {
+    public static void main(String[] args) {
+        ArithmeticInputOutput inputOutput = new ArithmeticInputOutput();
+        ArithmeticCalculator calculator = new ArithmeticCalculator();
+        
+        double x = inputOutput.getNumberFromUser();
+        double y = inputOutput.getNumberFromUser();
+        Operation operation = inputOutput.getOperationFromUser();
+        
+        double result = calculator.calculate(x, y, operation);
+        inputOutput.displayResult(result);
+    }
+}
